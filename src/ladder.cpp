41d0f27132fc41a5c8c4e3c934e9b684601099a9
Michael Chua <mchua3@uci.edu>
2025-03-15 21:18:54 +0000

    is start word is same with begin word return 0

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -40,7 +40,7 @@ bool is_adjacent(const string& word1, const string& word2) {
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
-    if (begin_word == end_word) return {begin_word};
+    if (begin_word == end_word) return {};
     if (word_list.find(end_word) == word_list.end()) return {};
 
     queue<vector<string>> ladder_queue;
