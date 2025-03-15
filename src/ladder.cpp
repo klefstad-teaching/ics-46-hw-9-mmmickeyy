2d7c42b1f54bc7f4cd4ad0cae2fb3dba7a2725bb
Michael Chua <mchua3@uci.edu>
2025-03-15 20:59:45 +0000

    fixed infinite loop

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -35,26 +35,28 @@ bool is_adjacent(const string& word1, const string& word2) {
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     if (begin_word == end_word) return {begin_word};
     if (word_list.find(end_word) == word_list.end()) return {};
-    
+
     queue<vector<string>> ladder_queue;
     ladder_queue.push({begin_word});
     set<string> visited;
+    visited.insert(begin_word);
 
     while (!ladder_queue.empty()) {
         vector<string> ladder = ladder_queue.front();
         ladder_queue.pop();
         string last_word = ladder.back();
 
-        if (last_word == end_word) return ladder;
-
         for (const auto& word : word_list) {
             if (is_adjacent(last_word, word) && !visited.count(word)) {
                 vector<string> new_ladder = ladder;
                 new_ladder.push_back(word);
+
+                if (word == end_word) return new_ladder;
+
                 ladder_queue.push(new_ladder);
+                visited.insert(word);
             }
         }
-        visited.insert(last_word);
     }
     return {};
 }
